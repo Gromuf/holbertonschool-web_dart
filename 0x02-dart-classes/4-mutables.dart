59a3936412477e08d670b40c80f195e56f20837d
Gromuf <luigi.beaumois@gmail.com>
2026-04-14 15:12:07 +0200

    Add Password class with validation and string representation methods

---
+++ 0x02-dart-classes/4-mutables.dart
@@ -0,0 +1,18 @@
+class Password {
+	String password = "";
+	Password({required this.password});
+	bool isValid() {
+		if (password.length < 8 || password.length > 16) return false;
+		if (!password.contains(RegExp(r'[A-Z]'))) return false;
+		if (!password.contains(RegExp(r'[a-z]'))) return false;
+		if (!password.contains(RegExp(r'[0-9]'))) return false;
+		return true;
+	}
+	@override
+	String toString() {
+		return "Your Password is: $password";
+	}
+
+	get getPassword => password;
+	set setPassword(String newPassword) => password = newPassword;
+}
