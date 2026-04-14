001b7a010f0194f1fe24d9799efc97364f680c58
Gromuf <luigi.beaumois@gmail.com>
2026-04-14 14:34:55 +0200

    Refactor inner function to include agent's initials and first name in greeting

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,6 +1,10 @@
 void outer(String name, String id) {
 	String inner() {
-		return "Hello Agent $name your id is $id";
+		var nameParts = name.split(" ");
+		var firstName = nameParts[0];
+		var lastName = nameParts[1];
+		var initial = lastName[0];
+		return "Hello Agent $initial.$firstName your id is $id";
 	}
 	print(inner());
 }
